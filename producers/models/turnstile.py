bd53cb97c153a30db8cd53adb595329378ad0c25
xaraujo <xavieraraujo@gmail.com>
2020-05-10 20:58:39 +0100

    Producers and images

---
+++ producers/models/turnstile.py
@@ -0,0 +1,59 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+
+logger = logging.getLogger(__name__)
+
+
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
+
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        super().__init__(
+            topic_name="org.chicago.cta.turnstiles",
+            key_schema=Turnstile.key_schema,
+            value_schema=Turnstile.value_schema,
+            num_partitions=1,
+            num_replicas=1,
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+        try:
+            num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+
+            for _ in range(num_entries):
+                self.producer.produce(
+                topic=self.topic_name,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id": self.station.station_id,
+                    "station_name": self.station.name,
+                    "line": self.station.color.name,
+                },
+            )
+        except Exception as e: 
+           logger.info("Turnstile didn't write into the topic {} - exception {}.".format(self.topic_name, e))
+
